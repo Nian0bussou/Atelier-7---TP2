083fe7080f137811f59f762a7aa6861d4c1a8210
アリス <161630589+Nian0bussou@users.noreply.github.com>
2024-09-30 11:22:31 -0400

    a

--- Assets/Scripts/Camera/DeplacementCamera.cs
+++ Assets/Scripts/Camera/DeplacementCamera.cs
@@ -6,8 +6,16 @@ public class DeplacementCamera : MonoBehaviour {
 
     void Update() {
         if (EstLiéeAuVaisseau) {
-            transform.position = Vaisseau.transform.position;
+            // À compléter
+            // Ce bout de code permet de modifier la position de la caméra de façon à ce qu'elle se soit toujours située au-dessus du vaisseau 
 
+            //transform.position = Vaisseau.transform.position; 
+
+            var pos = transform.position;
+            var vpos = transform.position;
+
+
+            transform.position = new(vpos.x, pos.y, vpos.z);
         }
     }
 
