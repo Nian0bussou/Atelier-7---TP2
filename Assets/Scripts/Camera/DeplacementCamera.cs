2b32d397760090a861f3b61f6b19adb117bd1b05
アリス <161630589+Nian0bussou@users.noreply.github.com>
2024-09-30 11:17:08 -0400

    2sdlfkj

--- Assets/Scripts/Camera/DeplacementCamera.cs
+++ Assets/Scripts/Camera/DeplacementCamera.cs
@@ -1,21 +1,18 @@
 using UnityEngine;
 
-public class DeplacementCamera : MonoBehaviour
-{
-   GameObject Vaisseau = null;
-   bool EstLiéeAuVaisseau = false;
-   void Update()
-   {
-      if (EstLiéeAuVaisseau)
-      {
-         // À compléter
-         // Ce bout de code permet de modifier la position de la caméra de façon à ce qu'elle se soit toujours située au-dessus du vaisseau 
-      }
-   }
+public class DeplacementCamera : MonoBehaviour {
+    GameObject Vaisseau = null;
+    bool EstLiéeAuVaisseau = false;
 
-   public void AffecterCaméraAuVaisseau(GameObject vaisseau)
-   {
-      Vaisseau = vaisseau;
-      EstLiéeAuVaisseau = true;
-   }
+    void Update() {
+        if (EstLiéeAuVaisseau) {
+            transform.position = Vaisseau.transform.position;
+
+        }
+    }
+
+    public void AffecterCaméraAuVaisseau(GameObject vaisseau) {
+        Vaisseau = vaisseau;
+        EstLiéeAuVaisseau = true;
+    }
 }
